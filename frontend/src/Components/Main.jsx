8661ac38eafc63138cf9bd38e09f318f0e55d840
Krutika <krutikavk@gmail.com>
2020-10-16 01:54:58 -0700

    Backend boilerplate + react setup

---
+++ frontend/src/Components/Main.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import {Route,Switch} from 'react-router-dom';
+
+// eslint-disable-next-line react/prefer-stateless-function
+class Main extends Component {
+  render() {
+    return (
+      <div>
+        Main component
+      </div>
+    );
+  }
+}
+
+export default Main;
