c831330149d51adb220443e7eefda9a8b8b23560
Krutika <krutikavk@gmail.com>
2020-11-05 18:07:36 -0800

    AWS deployment config

--- backend/Utils/config.js
+++ backend/Utils/config.js
@@ -1,6 +1,6 @@
 const config = {
   secret: 'cmpe273_secret_key',
-  frontendURL: 'http://localhost:3000',
+  frontendURL: process.env.REACT_APP_FRONTEND,
   mongoDB: 'mongodb+srv://admin:admin@yelp2.t9z29.mongodb.net/yelp-2?retryWrites=true&w=majority',
 };
 
--- backend/kafka/connection.js
+++ backend/kafka/connection.js
@@ -1,8 +1,12 @@
 const kafka = require('kafka-node');
+require('dotenv').config();
 
 function ConnectionProvider() {
   this.getConsumer = (topicName) => {
-    this.client = new kafka.Client('localhost:2181');
+    // this.client = new kafka.Client('localhost:2181');
+    console.log('kafka connect: ', process.env.REACT_APP_KAFKA);
+    this.client = new kafka.KafkaClient({ kafkaHost: process.env.REACT_APP_KAFKA });
+    // eslint-disable-next-line max-len
     this.kafkaConsumerConnection = new kafka.Consumer(this.client, [{ topic: topicName, partition: 0 }]);
     this.client.on('ready', () => { console.log('client ready!'); });
     return this.kafkaConsumerConnection;
@@ -10,9 +14,11 @@ function ConnectionProvider() {
 
   // Code will be executed when we start Producer
   this.getProducer = () => {
+    console.log('kafka connect: ', process.env.REACT_APP_KAFKA);
     if (!this.kafkaProducerConnection) {
       var HighLevelProducer = kafka.HighLevelProducer;
-      this.client = new kafka.Client('localhost:2181');
+      // this.client = new kafka.Client('localhost:2181');
+      this.client = new kafka.KafkaClient({ kafkaHost: process.env.REACT_APP_KAFKA });
       this.kafkaProducerConnection = new HighLevelProducer(this.client);
       // this.kafkaConnection = new kafka.Producer(this.client);
       console.log('producer ready');
